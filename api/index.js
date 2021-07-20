115a555b4abb64344fb9df7213408d8610079195
Mike <korostelevm@gmail.com>
2021-07-20 05:50:54 -0400

    asdf

--- api/index.js
+++ api/index.js
@@ -1,5 +1,29 @@
+const AWS = require("aws-sdk");
+AWS.config.update({
+  region: process.env.region || "us-east-2",
+});
+
+const publish = async function (msg) {
+   var params = {
+     Message: JSON.stringify(msg),
+     TopicArn: 'arn:aws:sns:us-east-2:348655018330:cyclic-per-region-init-AppLogsTopic-179X4JB0VLI5T',
+     MessageAttributes: {
+       app_id: {
+         DataType: "String",
+         StringValue: 'korostelevm-helloworld',
+       },
+     },
+   };
+   console.log(params)
+   await sns.publish(params).promise();
+ };
+ 
+ 
+
 module.exports.all = (req,res) => {
     var a = 'yodddd'
+    await publish({ts:Date.now(), line:'sadfasfd'});
+
     res.json({
        ... a,
        'asdfasdf':'asdfasdfasdfsdfasdfasfs',
